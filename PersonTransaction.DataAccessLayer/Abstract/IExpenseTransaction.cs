4b588bdc0b5641a316e8d832a3d7b8e91b909999
elifsudekvc <130573532+elifsudekvc@users.noreply.github.com>
2024-07-24 12:28:22 +0300

    interfaces added

---
+++ PersonTransaction.DataAccessLayer/Abstract/IExpenseTransaction.cs
@@ -0,0 +1,13 @@
+﻿using PersonTransaction.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonTransaction.DataAccessLayer.Abstract
+{
+    public interface IExpenseTransaction : IGenericDal<ExpenseTransaction>
+    {
+    }
+}
